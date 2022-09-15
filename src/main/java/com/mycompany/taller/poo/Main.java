934a064630258ba00260b9df7db7a0c8e7a14b63
Martin Borgo <martinborgo8@gmail.com>
2022-09-15 12:19:25 -0300

    Se agrego los elementos graficos basicos

--- src/main/java/com/mycompany/taller/poo/Main.java
+++ src/main/java/com/mycompany/taller/poo/Main.java
@@ -1,3 +1,5 @@
+package com.mycompany.taller.poo;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,5 +12,7 @@
 public class Main {
     public static void main(String []args){
         System.out.println("SOY EL MAIN");
+        
+        //jaja saludos
     }
 }
