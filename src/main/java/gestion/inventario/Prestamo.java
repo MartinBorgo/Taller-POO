520cec81b235cc6915e0f5e339c7412aaea6bea4
Leandro Molina <leandrorodrigomolina@gmail.com>
2022-10-22 15:54:18 -0300

    Se arreglo incremento solicitud facultad y general

--- src/main/java/gestion/inventario/Prestamo.java
+++ src/main/java/gestion/inventario/Prestamo.java
@@ -5,8 +5,7 @@
 package gestion.inventario;
 
 import enumeraciones.PrestamoTipo;
-import gestion.personas.Lector;
-import gestion.personas.Usuario;
+import gestion.personas.*;
 import java.io.Serializable;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -44,9 +43,12 @@ public class Prestamo implements Serializable{
         this.fechaDevolucion = new GregorianCalendar(fechaInicio.get(Calendar.YEAR), fechaInicio.get(Calendar.MONTH), fechaInicio.get(Calendar.DAY_OF_MONTH) + diasDePrestamo);
     
         // Se incrementa el contador de obra de acuerdo al tipo de lector general/profesor/alumno
-        if(lectorSolicita instanceof Lector) {
+        
+        if(lectorSolicita.getClass().getName().equals("gestion.personas.Lector")) {
             ejemplarSolicitado.getObra().incrementarGeneral();
-        } else {
+        } else if (lectorSolicita.getClass().getName().equals("gestion.personas.Alumno")) {
+            ejemplarSolicitado.getObra().incrementarFacultad();
+        }else if(lectorSolicita.getClass().getName().equals("gestion.personas.Docente")){
             ejemplarSolicitado.getObra().incrementarFacultad();
         }
         
