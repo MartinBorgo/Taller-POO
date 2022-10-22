7f14734ae4369942515c20f22b46872a6d55a8d9
Martin Borgo <martinborgo8@gmail.com>
2022-10-22 15:48:02 -0300

    Arreglos menores

---
+++ src/main/java/excepciones/EjemplarEnPrestamoError.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+/**
+ *
+ * @author martin
+ */
+public class EjemplarEnPrestamoError extends Exception{
+
+    public EjemplarEnPrestamoError(String message) {
+        super(message);
+    }
+    
+    public EjemplarEnPrestamoError() {
+    }
+
+    
+}
